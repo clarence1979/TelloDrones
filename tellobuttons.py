67181d04f628a400bfa0d66e5ccbf20949f3d69d
Clarence <47097322+clarence1979@users.noreply.github.com>
2022-11-07 23:32:30 +1100

    Update tellobuttons.py

--- tellobuttons.py
+++ tellobuttons.py
@@ -21,12 +21,18 @@ def left1():
     tello.move_left(100) #values 20-500cm
 def right1():    
     tello.move_right(100)
+def takepic1():
+    #Take picture
+    tello.streamon()
+    frame_read = tello.get_frame_read()
+    cv2.imwrite("picture.png", frame_read.frame)
 
 tkinter.Label(window, text = "Tello Controls").pack()
 tkinter.Button(window, text = "Takeoff", command = takeoff1).pack()
 tkinter.Button(window, text = "Land!", command = land1).pack()
 tkinter.Button(window, text = "Left", command = left1).pack()
 tkinter.Button(window, text = "Right", command = right1).pack()
+tkinter.Button(window, text = "Take Pic", command = takepic1).pack()
 
 window.geometry('200x200+150+150')
 window.minsize(150,150)
