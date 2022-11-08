7656dbd6ca25f5933ecbe8305946b0fb66cd4e80
Clarence <47097322+clarence1979@users.noreply.github.com>
2022-11-09 01:13:53 +1100

    Update Lab10_yr9_SectionA_unmodified.py

--- Lab10_yr9_SectionA_unmodified.py
+++ Lab10_yr9_SectionA_unmodified.py
@@ -1,5 +1,8 @@
-from djitellopy import Tello 
-import cv2 
+from djitellopy import Tello #Library to enable Tello to run
+import cv2 #Library for taking photos and image recognition
+
+#STUDENT TO WRITE ALL COMMENTS USING THE HASH NOTATION FOR THE REST OF THE CODE BELOW AND SUBMIT VIA SEQTA
+#CODE NEEDS TO BE TESTED ON A DRONE. RECORD VIDEO OF SUCCESSFUL TESTING AND UPLOAD TO SEQTA.
 
 import time
 
